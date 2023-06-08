fe4b186765867f044a6e589a1125115970c9d31c
Martin Martin <martin@martincmartin.com>
2023-06-08 15:26:05 -0400

    Move add_comm_group stuff to its own class.

--- src/chapter1/section1B.lean
+++ src/chapter1/section1B.lean
@@ -6,25 +6,52 @@ import algebra.field.basic
 import init.data.fin.basic
 import group_theory.group_action.pi
 
-
 -- 1.19  Definition  vector space
 
 -- In LADR, a vector space is a set V and field F, along with two functions,
 -- addition and scalar multiplication of vectors.  It also requries that certain
 -- properties hold.  Some of those properties include "there exists," for
 -- example "∀ v : V, ∃ w : V, v + w = 0."  Lean prefers an alternate approach,
--- where we provide a function which, given v, returns w.
+-- where instead of "there exists" properties, we provide a function which,
+-- given v, returns w.
+
+-- Lean has a vary useful ability to take *implicit arguments*.  For example,
+-- when you want to apply the theorem "0 + v = v" to a particular v, you don't
+-- need to explicitly specify the vector_space, it can usually infer it.  Not
+-- having to explicitly write the vector_space everywhere saves a lot of reading
+-- & generally focuses on what's important, rather than what's understood.  For
+-- example, in Linear Algebra Done Right, Axler says "from now on, V is a vector
+-- space" and then doesn't need to repeat it over and over for each theorm.
+
+-- Some theorems don't depend on the field of scalars and will work with any
+-- field, e.g. the theorm that the negative (addative inverse) of a vector is
+-- unique.  In this case, Lean can go looking for the field, but since any field
+-- will do, it won't have a lot of guideance and can get stuck.  And anyway this
+-- work is useless, since the field isn't even used in the proof.
+
+-- So, we separate out the parts that involve only addition, including the zero
+-- and negation, into a class of their own that doesn't even mention field.  In
+-- abstract algebra, this is an additive commutative group, so we call it
+-- add_comm_group.
+
+-- Put this in its own namespace, since there is already an add_comm_group in
+-- mathlib, and it gets included when we include field.
+namespace LADR
 
 -- "extends has_add" adds a function 'add' to vector_space that uses infix
--- notation, e.g. 'u + v'.  Similarly with the other "extends" classes.
+-- notation, e.g. 'u + v'.  Similarly with the other "extends" clauses.
 @[ext]
-class vector_space (F : Type*) (V : Type*) [field F]
-  extends has_add V, has_smul F V, has_zero V, has_neg V :=
+class add_comm_group (V : Type*) extends has_add V, has_zero V, has_neg V :=
 (add_comm : ∀ u v : V, u + v = v + u)
 (add_assoc : ∀ u v w : V, (u + v) + w = u + (v + w))
-(smul_assoc : ∀ a b : F, ∀ v : V, (a * b) • v = a • (b • v))
 (add_zero : ∀ v : V, v + 0 = v)
 (add_right_inv : ∀ v : V, v + -v = 0)
+
+
+@[ext]
+class vector_space (F : Type*) (V : Type*) [field F] [add_comm_group V]
+  extends has_smul F V :=
+(smul_assoc : ∀ a b : F, ∀ v : V, (a * b) • v = a • (b • v))
 (mul_ident : ∀ v : V, (1 : F) • v = v)
 (left_distrib : ∀ a : F, ∀ u v : V, a • (u + v) = (a • u) + (a • v))
 (right_distrib : ∀ a b :F, ∀ v : V, (a + b) • v = (a • v) + (b • v))
@@ -33,17 +60,20 @@ class vector_space (F : Type*) (V : Type*) [field F]
 
 variables {F : Type*} [field F] {n : ℕ}
 
--- Should this use instance or def?
-def n_tuple_vector_space : vector_space F (fin n → F) :=
-{ add  := λ u v, u + v,
-  smul := λ a v, a • v,
+instance n_tuple_add_comm_group : add_comm_group (fin n → F) :=
+{ add := λ u v, u + v,
   zero := 0,
   neg := λ v, - v,
   add_comm := add_comm,
   add_assoc := add_assoc,
-  smul_assoc := smul_assoc,
   add_zero := add_zero,
-  add_right_inv := add_neg_self,
+  add_right_inv := add_neg_self
+}
+
+-- Should this use instance or def?
+def n_tuple_vector_space : vector_space F (fin n → F) :=
+{ smul := λ a v, a • v,
+  smul_assoc := smul_assoc,
   mul_ident := one_smul F,
   left_distrib := 
   begin
@@ -66,16 +96,19 @@ def n_tuple_vector_space : vector_space F (fin n → F) :=
 -- We defined an n-tuple as a map from `fin n` to F, so a natural definition of
 -- an infinite tuple is just a map from ℕ to F.
 
-def inf_tuple_vector_space : vector_space F (ℕ → F) :=
-{ add  := λ u v, u + v,
-  smul := λ a v, a • v,
+instance inf_tuple_add_comm_group : add_comm_group (ℕ → F) :=
+{ add := λ u v, u + v,
   zero := 0,
   neg := λ v, - v,
   add_comm := add_comm,
   add_assoc := add_assoc,
-  smul_assoc := smul_assoc,
   add_zero := add_zero,
-  add_right_inv := add_neg_self,
+  add_right_inv := add_neg_self
+}
+
+def inf_tuple_vector_space : vector_space F (ℕ → F) :=
+{ smul := λ a v, a • v,
+  smul_assoc := smul_assoc,
   mul_ident := one_smul F,
   left_distrib := 
   begin
@@ -99,16 +132,19 @@ def inf_tuple_vector_space : vector_space F (ℕ → F) :=
 
 -- 1.24  Example  F^S is a vector space
 variable S : Type*
-def fun_vector_space : vector_space F (S → F) :=
-{ add  := λ u v x, u x + v x,
-  smul := λ a v x, a * v x,
-  zero := λ x, 0,
-  neg := λ v x, - v x,
+instance fun_add_comm_group : add_comm_group (S → F) :=
+{ add := λ u v, u + v,
+  zero := 0,
+  neg := λ v, - v,
   add_comm := add_comm,
   add_assoc := add_assoc,
-  smul_assoc := smul_assoc,
   add_zero := add_zero,
-  add_right_inv := add_neg_self,
+  add_right_inv := add_neg_self
+}
+
+def fun_vector_space : vector_space F (S → F) :=
+{ smul := λ a v x, a * v x,
+  smul_assoc := smul_assoc,
   mul_ident := one_smul F,
   left_distrib := 
   begin
@@ -123,55 +159,49 @@ def fun_vector_space : vector_space F (S → F) :=
     apply field.right_distrib
   end}
 
-namespace vector_space
 
+namespace vector_space
 
 -- 1.25  Unique additive identity
 -- "A vector space has a unique additive identity"
 
-theorem unique_add_ident {V : Type*} [vector_space F V] {z : V}:
-  (∀ v : V, v + z = v) → z = 0 :=
-begin
-  intros h,
-  calc z = z + 0 : by rw add_zero
-  ...    = 0 + z : by rw add_comm
+theorem unique_add_ident {V : Type*} [add_comm_group V] {z : V} (h: ∀ v : V, v + z = v):
+  z = 0 :=
+  calc z = z + 0 : by rw add_comm_group.add_zero
+  ...    = 0 + z : by rw add_comm_group.add_comm
   ...    = 0     : by rw h
-end
 
 
 -- 1.26  Unique additive inverse
 -- "Every element in a vector space has a unique additive inverse"
 
-lemma zero_add {V : Type*} [vector_space F V] {v : V} : 0 + v = v :=
+lemma zero_add {V : Type*} [add_comm_group V] {v : V} : 0 + v = v :=
 begin
-  rw add_comm,
-  apply add_zero
+  rw add_comm_group.add_comm,
+  apply add_comm_group.add_zero
 end
 
-theorem unique_add_inv (V : Type*) [vector_space F V] (v w : V):
-  v + w = 0 → w = -v :=
-begin
-  intro h,
-  calc w = w + 0 : by rw vector_space.add_zero
-  ...    = w + (v + -v) : by rw ← add_right_inv
-  ...    = (w + v) + -v : by rw add_assoc
-  ...    = (v + w) + -v : by rw add_comm w v
+theorem unique_add_inv {V : Type*} [add_comm_group V] {v w : V} (h : v + w = 0):
+  w = -v :=
+  calc w = w + 0 : by rw add_comm_group.add_zero
+  ...    = w + (v + -v) : by rw ← add_comm_group.add_right_inv
+  ...    = (w + v) + -v : by rw add_comm_group.add_assoc
+  ...    = (v + w) + -v : by rw add_comm_group.add_comm w v
   ...    = 0 + -v : by rw h
-  ...    = -v : zero_add
-end
+  ...    = -v : by rw zero_add
 
 
 -- 1.27  Notation  -v, w - v
 -- We started with -v, before proving it was unique.
 
 -- Notation for 'w - v'
-instance {V : Type*} [vector_space F V] : has_sub V := ⟨λ w v, w + (-v)⟩
-@[simp] lemma sub_add_neg {V : Type*} [vector_space F V] (u v : V) :
+instance {V : Type*} [add_comm_group V] : has_sub V := ⟨λ w v, w + (-v)⟩
+@[simp] lemma sub_add_neg {V : Type*} [add_comm_group V] (u v : V) :
   u - v = u + (-v) := rfl
 
 
 -- 1.28  Notation  V
-variables {V : Type*} [vector_space F V]
+variables {V : Type*} [add_comm_group V] [vector_space F V]
 
 
 -- 1.29  The number 0 times a vector
@@ -179,21 +209,19 @@ variables {V : Type*} [vector_space F V]
 -- Lean proofs are very explicit, so we tend to use lots of helper lemmas for
 -- things that most mathematicians wouldn't even mention in their proofs.
 
--- Why do I need to specify [vector_space F V] here, when I've said it above?
--- How can I avoid saying it in the preamble of every theorem?
-theorem sub_self [vector_space F V] {v : V} : v - v = 0 :=
-  by {simp, apply add_right_inv}
+theorem sub_self {v : V} : v - v = 0 :=
+  by {simp, apply add_comm_group.add_right_inv}
 
-theorem add_sub_cancel [vector_space F V] {u v : V} : (u + v) - v = u :=
+theorem add_sub_cancel {u v : V} : (u + v) - v = u :=
 begin
   simp,
-  rw [add_assoc, add_right_inv, add_zero]
+  rw [add_comm_group.add_assoc, add_comm_group.add_right_inv, add_comm_group.add_zero]
 end
 
 -- All the (0 : F) here is really distracting.  Is there a way to get Lean to
 -- understand that in 0 • v, the zero is not a natural number but from the
 -- field?
-theorem zero_vec_eq_zero [vector_space F V] {v : V} : (0 : F) • v = 0 :=
+theorem zero_vec_eq_zero {v : V} : (0 : F) • v = 0 :=
 begin
   apply eq.symm,
   calc 0 = (0 : F) • v - (0 : F) • v : by rw sub_self
@@ -205,19 +233,19 @@ end
 
 -- 1.30  A number times the vector 0
 
-theorem field_zero_eq_zero [vector_space F V] {a : F} : a • (0 : V) = 0 :=
+theorem field_zero_eq_zero {a : F} : a • (0 : V) = 0 :=
 begin
   calc
     a • (0 : V) = a • 0 + a • 0 - a • 0 : by rw add_sub_cancel
     ...         = a • (0 + 0) - a • 0 : by rw left_distrib
-    ...         = a • 0 - a • 0 : by rw add_zero
+    ...         = a • 0 - a • 0 : by rw add_comm_group.add_zero
     ...         = 0 : by rw sub_self
 end
 
 
 -- 1.31  The number -1 times a vector
 
-theorem neg_one_smul_is_neg [vector_space F V] {v : V} : (-1 : F) • v = - v :=
+theorem neg_one_smul_is_neg {v : V} : (-1 : F) • v = - v :=
 begin
   rw ← unique_add_inv,
   calc v + (-1 : F) • v = (1 : F) • v + (-1 : F) • v : by rw mul_ident
@@ -228,3 +256,4 @@ begin
 end
 
 end vector_space
+end LADR
